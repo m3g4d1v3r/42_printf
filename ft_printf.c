9165ffb6f5347553357d165af89c0f2ac9942a7b
m3g4d1v3r <marlon365x@protonmail.ch>
2022-07-31 16:30:45 -0300

    More on norm refactoring: ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,34 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:08:37 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/07/31 13:05:37 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/07/31 16:32:53 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void	ft_offstate(t_state *fmt_st, char chr, int *chr_c)
+{
+	if (chr == '%')
+		*fmt_st = ON;
+	else
+	{
+		write(1, &chr, 1);
+		(*chr_c)++;
+	}
+}
+
+static void	ft_onstate(va_list *args_ptr, t_state *fmt_st, char chr, int *chr_c)
+{
+	ft_parse_chr(args_ptr, fmt_st, chr, chr_c);
+	ft_parse_str(args_ptr, fmt_st, chr, chr_c);
+	ft_parse_int(args_ptr, fmt_st, chr, chr_c);
+	ft_parse_uint(args_ptr, fmt_st, chr, chr_c);
+	ft_parse_ptr(args_ptr, fmt_st, chr, chr_c);
+	ft_parse_hex(args_ptr, fmt_st, chr, chr_c);
+	if (*fmt_st == DONE)
+		*fmt_st = OFF;
+}
 
 int	ft_printf(const char *format_str, ...)
 {
@@ -24,26 +47,9 @@ int	ft_printf(const char *format_str, ...)
 	while (*format_str != '\0')
 	{
 		if (fmt_st == OFF)
-		{
-			if (*format_str == '%')
-				fmt_st = ON;
-			else
-			{
-				write(1, format_str, 1);
-				chr_counter++;
-			}
-		}
+			ft_offstate(&fmt_st, *format_str, &chr_counter);
 		else
-		{
-			ft_parse_chr(&args, &fmt_st, *format_str, &chr_counter);
-			ft_parse_str(&args, &fmt_st, *format_str, &chr_counter);
-			ft_parse_int(&args, &fmt_st, *format_str, &chr_counter);
-			ft_parse_uint(&args, &fmt_st, *format_str, &chr_counter);
-			ft_parse_ptr(&args, &fmt_st, *format_str, &chr_counter);
-			ft_parse_hex(&args, &fmt_st, *format_str, &chr_counter);
-			if (fmt_st == DONE)
-				fmt_st = OFF;
-		}
+			ft_onstate(&args, &fmt_st, *format_str, &chr_counter);
 		format_str++;
 	}
 	va_end(args);
